d9135254e9c48655baff35117e000b8ba266e1d5
Margaret Ossman <mrsmargareto@gmail.com>
2024-08-14 08:44:43 -0500

    Working on format

--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -1,17 +1,9 @@
 @props(['page'])
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    @livewireStyles
+<x-filament-fabricator::layouts.base :title="$page->title">
+    {{-- Header Here --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
     <x-filament-fabricator::page-blocks :blocks="$page->blocks" />
-</body>
 
-</html>
+    {{-- Footer Here --}}
+</x-filament-fabricator::layouts.base>
